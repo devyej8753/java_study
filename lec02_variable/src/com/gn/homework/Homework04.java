86de32c45cf437d433a9d08102ec105e5a1e57b1
devyej8753 <devyej8753@gmail.com>
2024-11-27 18:43:48 +0900

    [lyj] Homework

--- lec02_variable/src/com/gn/homework/Homework04.java
+++ lec02_variable/src/com/gn/homework/Homework04.java
@@ -5,7 +5,7 @@ public class Homework04 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("문자를 입력하세요 :");
-		String Word = sc.nextLine();
+		String score = sc.nextLine();
 		char su1 ='A';
 		int su2 = su1;		
 		System.out.println("A의 유니코드 :"+su2);
--- lec02_variable/src/com/gn/homework/Homework06.java
+++ lec02_variable/src/com/gn/homework/Homework06.java
@@ -1,8 +1,18 @@
 package com.gn.homework;
-
+import java.util.Scanner;
 public class Homework06 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("한 자리 숫자를 입력하세요:");
+		String nub1 = sc.nextLine();
+		char a = nub1.charAt(0);
+		int b = a - '0';
+		int c = b * b;
+		System.out.println("입력한 숫자의 제곱은"+c +"입니다");
+		
+		
+		
 /*	## 1) 문제
 	사용자로부터 한 자리 숫자를 입력 받아서
 	해당 숫자의 제곱을 출력하는 프로그램을 작성하세요.
@@ -10,7 +20,7 @@ public class Homework06 {
 	char 타입의 변수에 저장하세요.
 	💡 문자열 형태로 입력받은 한자리 값을 char타입의 변수로 변환하는 코드는 sc.nextLine().charAt(0); 입니다.
 */
-
+		
 
 	}
 
