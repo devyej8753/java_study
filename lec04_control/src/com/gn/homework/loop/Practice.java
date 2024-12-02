0e4b1a7e19f9a6a49bd88b7e5d4b021eb0733bac
이예준 <@DESKTOP-JM1BELL>
2024-12-03 00:10:05 +0900

    [lyj] 2024.12.03 home commit

--- lec04_control/src/com/gn/homework/loop/Practice.java
+++ lec04_control/src/com/gn/homework/loop/Practice.java
@@ -1,8 +1,119 @@
 package com.gn.homework.loop;
 
+import java.util.Scanner;
+
 public class Practice {
-	public void practice02() {
+	public void practice01() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("정수(1~50) : ");
+		int num = sc.nextInt();
+		int sum = 0;
+		while(true) {
+			
+			if(1<=num&&num<=50) {
+				for(int i=1; i<=num; i++) {
+					if(i%2==0) {
+						sum += i;
+					}
+				}
+				System.out.println("합계 : " + sum);
+				sc.close();
+				break;
+			} else {
+				System.out.println("1~50 사이의 정수를 입력하세요.");
+				System.out.print("정수(1~50) : ");
+				num = sc.nextInt();
+			}
+		}
 		
 	}
+	public void practice02() {
+		int i = 0;
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			System.out.print("숫자 : ");
+			i = sc.nextInt();
+			if(i==0) {
+				break;
+			}else if(i>9) {
+				System.out.println("9 이하의 숫자만 입력해주세요.");
+			}
+			for(;i<=9;i++) {
+				if(i<0) {
+					System.out.println("양수만 입력해주세요.");
+					break;
+				}
+			System.out.println("=== "+i+"===");	
+			for(int j = 1 ;j<=9;j++) {
+				System.out.println(i+" * "+j+" = "+(i*j));
+				}
+			}
+		}
+		sc.close();
+		
+	}
+	public void practice03() {
+		Scanner sc = new Scanner(System.in);
+		
+		for(;;) {
+			System.out.print("입력 : ");
+			String name = sc.nextLine();
+			
+			if(name.equals("탈출")) {
+				System.out.println("프로그램 종료");
+				sc.close();
+				break;
+			}
+			System.out.println(name);
+	}
+		
+		}
+		
+	public void practice04() {
+		Scanner sc = new Scanner(System.in);
+		String sum;
+		
+		do {
+			System.out.println("게임을 실행합니다!");
+			System.out.println("게임을 다시 플레이하시겠습니까?(yes/no)");
+			sum = sc.nextLine();
+		} while(sum.equals("yes"));
+		System.out.println("게임을 종료합니다.");
+		sc.close();
+		
+		}
+	
 
+		
+	public void practice05() {
+		Scanner sc = new Scanner(System.in);
+		int sums = 0;
+		int number;
+		
+		while(true) {
+			System.out.print("숫자(0을 입력하면 종료) : ");
+			number = sc.nextInt();
+			if (number == 0) {
+				break;
+			}else if (number > 0) {
+				sums += number;
+			}
+			
+		}
+		System.out.println("양수의 합계 :" + sums);
+		sc.close();
+	}
+	
+	public void practice06() {
+		
+	}
+	public void practice07() {
+		
+	}
+	public void practice08() {
+		
+	}
 }
+
+	
--- lec04_control/src/com/gn/homework/loop/Run.java
+++ lec04_control/src/com/gn/homework/loop/Run.java
@@ -6,14 +6,32 @@ public class Run {
 
 	public static void main(String[] args) {
 		System.out.println("실행할 기능을 입력하세요.");
+		System.out.println("1. 1~50까지 짝수 합하기");
 		System.out.println("2. 구구단 무한반복");
 		System.out.println("3. 탈출합시다.");
+		System.out.println("4. 게임 재시작");
+		System.out.println("5. 양수만 더하기");
 		System.out.println("6. 트리만들기");
 		System.out.println("7. 박수박수박");
+		System.out.println("8. 문자열의 개수");
 		System.out.print("선택 : ");
 		
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
+		int menu = sc.nextInt();
+		
+		Practice practice = new Practice();
+		switch(menu) {
+			case 1 :practice.practice01();break;
+			case 2 :practice.practice02();break;
+			case 3 :practice.practice03();break;
+			case 4 :practice.practice04();break;
+			case 5 :practice.practice05();break;
+			case 6 :practice.practice06();break;
+			case 7 :practice.practice07();break;
+			case 8 :practice.practice08();break;
+		//	default:
+				
+		}
 		
 		
 
