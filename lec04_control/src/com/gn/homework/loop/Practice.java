b3dcc480b14f053a12b288ff1d8998b28fd73178
devyej8753 <devyej8753@gmail.com>
2024-12-03 19:48:41 +0900

    [lyj] 2024.12.03 반복문 Commit

--- lec04_control/src/com/gn/homework/loop/Practice.java
+++ lec04_control/src/com/gn/homework/loop/Practice.java
@@ -106,16 +106,114 @@ public class Practice {
 	}
 	
 	public void practice06() {
-		Scanner sc = new Scanner(System.in);
+		for(int i = 0 ; i <5 ; i++) {
+			for(int j = 0 ; j < 4-i ; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0 ; j < 2*i+1 ; j++) {
+				System.out.print("*");
+			}
+				System.out.println(" ");
+		}
 		
 		
 	}
 	public void practice07() {
+		Scanner sc = new Scanner(System.in);
+		int start = 0;
+		while(true) {
+			System.out.print("정수 : ");
+			start = sc.nextInt();
+			if(start<=0) {
+				System.out.println("양수가 아닙니다. ");
+			}else {
+				for(int i = 1 ; i <= start ; i++) {
+					if(i%2==0) {
+						System.out.print("수");
+					}else {
+						System.out.print("박");
+					}
+				}
+				break;
+			}
+			
+			}
+			
+		}
 		
-	}
+		
+		
+		
+	
 	public void practice08() {
+		Scanner sc = new Scanner(System.in);
+		String str;
+		char chr;
+		do {
+			System.out.print("문자열 : ");
+			str = sc.nextLine();
+			
+			System.out.print("문자 : ");
+			char ch = sc.next().charAt(0);
+			
+			int count = 0;
+			for(int i = 0; i<str.length();i++) {
+				if(str.charAt(i)==ch) {
+					count++;
+				}
+			}
+			
+			System.out.println("포함된 개수 : "+count);
+			
+			do {
+				System.out.print("다시 입력하시겠습니까? : ");
+				chr = sc.next().charAt(0);
+				if(chr!='N'&&chr!='n'&&chr!='Y'&&chr!='y') {
+					System.out.println("잘못된 대답입니다. 다시 입력해주세요.");
+				}
+			}while(chr!='N'&&chr!='n'&&chr!='Y'&&chr!='y');
+			
+			sc.nextLine();	
+		}while(chr=='Y'||chr=='y');
+		sc.close();
+			
+		}
 		
 	}
-}
+	
+	
+		
+
+
 
 	
+//		int sum = 0;
+//		int count = 0;
+//		
+//		while(true) {
+//			System.out.print("문자열 : ");
+//			String str =sc.nextLine();
+//			System.out.print("문자 : ");
+//			String ch =sc.nextLine();
+//			for(int i = 0; i<str.length(); i++) {
+//				
+//				if(ch.charAt(0)==str.charAt(i));{
+//					count++;
+//				}
+//			}
+//			System.out.println("포함된 개수 : "+count);
+//			System.out.print("다시 입력하시겠습니까? : ");
+//			String answer = sc.nextLine();
+//			if(answer.equals("N")||answer.equals("n")) {
+//				break;
+//			}else if(answer.equals("Y")||answer.equals("y")) {
+//				System.out.print("문자열 : ");
+//				str = sc.nextLine();
+//				System.out.print("문자 : ");
+//				str = sc.nextLine();
+//				break;
+//			}else {
+//				System.out.println("잘못된 대답입니다. 다시 입력해주세요");
+//				
+//			}
+//			sc.close();
