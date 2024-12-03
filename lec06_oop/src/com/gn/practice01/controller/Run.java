742dad30bb58e9aaef3eb99d4dca2b10df5ed7e3
devyej8753 <devyej8753@gmail.com>
2024-12-04 08:57:45 +0900

    [lyj] oop 최초 commit

---
+++ lec06_oop/src/com/gn/practice01/controller/Run.java
@@ -0,0 +1,16 @@
+package com.gn.practice01.controller;
+
+import com.gn.practice01.model.vo.Person;
+
+public class Run {
+	public static void main(String[] args) {
+		//클래스 생성구문을 활용해서
+		//Run 클래스에 Person 클래스를 인스턴스화 하세요.
+		//(클래스명 참조변수명 = new 클래스명();)
+		Person p = new Person();
+		
+		
+
+	}
+
+}
---
+++ lec06_oop/src/com/gn/study/basic/model/vo/Car.java
@@ -0,0 +1,8 @@
+package com.gn.study.basic.model.vo;
+
+// public(같은 패키지+프로젝트 전체) - 접근제한자
+// class - 예약어
+// car - 클래스명
+public class Car {
+
+}
