f7e818280d95e1b9c4fa26f7c83893376f77b96c
이예준 <@DESKTOP-JM1BELL>
2024-12-07 23:22:10 +0900

    [lyj] homework

--- lec06_oop/src/com/gn/homework07/controller/Run.java
+++ lec06_oop/src/com/gn/homework07/controller/Run.java
@@ -1,7 +1,13 @@
 package com.gn.homework07.controller;
 
+import com.gn.homework07.model.vo.Employee;
+
 public class Run {
 	public static void main(String[] args) {
+		Employee e = new Employee();
+		
+		
+		
 		
 	}
 
--- lec06_oop/src/com/gn/homework07/model/vo/Employee.java
+++ lec06_oop/src/com/gn/homework07/model/vo/Employee.java
@@ -11,5 +11,76 @@ public class Employee {
 	private double bonus;
 	private String phone;
 	private String address;
+	
+	
+	
+	public int getEmpNo() {
+		return empNo;
+	}
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public double getBonus() {
+		return bonus;
+	}
+	public void setBonus(double bonus) {
+		this.bonus = bonus;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public Employee() {}
+	public Employee(int empNo, String empName) {}
+	public Employee(int empNo, String empName, String dept, String job, int age, char gender,
+			int salary, double bonus, String phone, String address) {
+		
+	}
+
 
 }
