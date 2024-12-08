7ba97e098cc3bdc6f7fd79a767e0a10df5efb2ed
이예준 <@DESKTOP-JM1BELL>
2024-12-08 14:45:56 +0900

    [lyj] 객체 3번 commit

--- lec06_oop/src/com/gn/homework07/model/vo/Employee.java
+++ lec06_oop/src/com/gn/homework07/model/vo/Employee.java
@@ -76,9 +76,23 @@ public class Employee {
 	}
 	
 	public Employee() {}
-	public Employee(int empNo, String empName) {}
+	public Employee(int empNo, String empName) {
+		this.empNo = empNo;
+		this.empName = empName;
+	}
 	public Employee(int empNo, String empName, String dept, String job, int age, char gender,
 			int salary, double bonus, String phone, String address) {
+		this.empNo = empNo;
+		this.empName = empName;
+		this.dept = dept;
+		this.job = job;
+		this.age = age;
+		this.gender = gender;
+		this.salary = salary;
+		this.bonus = bonus;
+		this.phone = phone;
+		this.address = address;
+		
 		
 	}
 
