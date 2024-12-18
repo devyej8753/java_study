c5bd88a0b606f19131465dc4688c7d8fa63dca65
devyej8753 <devyej8753@gmail.com>
2024-12-18 19:21:11 +0900

    [lyj] homework11

--- lec12_api/src/com/gn/homework11/Run.java
+++ lec12_api/src/com/gn/homework11/Run.java
@@ -2,15 +2,33 @@ package com.gn.homework11;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
 public class Run {
 	public static void main(String[] args) {
-		LocalTime start = LocalTime.now();
-		DateTimeFormatter dtf1 	= DateTimeFormatter.ofPattern("HH시 mm분 ss초");
-		System.out.println("기준 : "+dtf1.format(start));
 		
-		Scanner sc = new Scanner(System.in);
+		LocalTime start = LocalTime.now();
+		DateTimeFormatter dtf 	= DateTimeFormatter.ofPattern("HH시 mm분 ss초");
+		System.out.println("기준 : "+dtf.format(start));
+		for(int i = 0 ; ; i++) {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("입력 : ");
+			String arr = sc.next();
+			arr = arr.toUpperCase();
+			// 새로 LocalTime after 선언
+			LocalTime last = LocalTime.now();
+			DateTimeFormatter dtf1 	= DateTimeFormatter.ofPattern("ss");
+			long daysBetween = ChronoUnit.SECONDS.between(start, last);
+			if("Y".equals(arr)) {
+				System.out.println("경과(초) : "+daysBetween); // after-start
+			}else if("N".equals(arr)) {
+				System.out.println("종료합니다.");
+				break;
+			}else {
+				System.out.println("다시 입력해주세요.");
+			}
+		}
 		
 		
 		
