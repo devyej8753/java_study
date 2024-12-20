26a5ba70787146f2f96d52ae1ea9549bac34252d
devyej8753 <devyej8753@gmail.com>
2024-12-20 10:46:43 +0900

    [lyj] !

--- lec13_io/src/com/gn/homework03/model/dao/FileDAO.java
+++ lec13_io/src/com/gn/homework03/model/dao/FileDAO.java
@@ -1,38 +1,39 @@
 package com.gn.homework03.model.dao;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class FileDAO {
+	
+	String total = new String("C:\\homework3");
+	
 	public boolean checkName(String file) {
-		File checkFile = new File("C:\\test\\sub",file);
+		File checkFile = new File(total,file);
 		if(checkFile.exists() != false) {
 			checkFile.mkdirs();
 		}
 		return checkFile.exists();
 	}
 	public void fileSave(String file, String s) {
-		File savefile = new File(file);
-		try(BufferedInputStream in = new BufferedInputStream(new FileInputStream(file))){
-			byte[] arr = new byte[(int)savefile.length()];
-			int idx = 0;
-			int date;
-			while(true) {
-				date = in.read();
-				if(date == -1)break;
-				arr[idx++] = (byte)date;
-			}
-			String save = new String(s);
+		File savefile = new File(total);
+//		if (savefile.exists()==false) {
+//			savefile.mkdirs();
+//		}
+		File arr = new File(savefile,file);
+		try(BufferedOutputStream save = new BufferedOutputStream(new FileOutputStream(arr))){
+			byte[] num = s.getBytes();
+			save.write(num);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} 
-		
-		
-		
+
 	}
+		
+	
 	public StringBuilder fileOpen(String file) {
 		return null;
 	}
